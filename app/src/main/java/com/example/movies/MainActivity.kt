ca5335588b30c024b49828f08ece81e00d63c2df
trishadubey39 <trishadubey39@gmail.com>
2023-09-05 17:21:33 +0530

    bindables-depenndancy

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.VisibleForTesting
+import com.example.movies.databinding.ActivityMainBinding
+import com.example.movies.movieList.MovieListViewModel
+import com.skydoves.bindables.BindingActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    @get:VisibleForTesting
+    internal val viewModel: MovieListViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding {
+            adapter = PokemonAdapter()
+            vm = viewModel
+        }
     }
 }
